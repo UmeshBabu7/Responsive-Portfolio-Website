ddbfe65ef5466260e708f3d1e6d50f1a5ef8791a
Umesh <umeshtamang129@gmail.com>
2025-06-19 13:10:38 +0545

    update

--- portfolio/Base/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from Base.models import Contact 
-
-# Register your models here.
-class ContactAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email','content','number')
-
-admin.site.register(Contact,ContactAdmin)
